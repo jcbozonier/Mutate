60a0b5a937fd2b6a3cd48a9dbe5b799e1aecb27f
unknown <DarkWindows@.(none)>
2010-07-11 00:52:41 -0700

    Initial check in with dead simple VS2010 project to act as first story.

---
+++ SampleDotNetProject/SampleCodeLibrary/Foo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCodeLibrary
+{
+    public class Foo
+    {
+        public bool GetResult(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public bool GetTestedResult(bool value)
+        {
+            return !value;
+        }
+    }
+}
---
+++ SampleDotNetProject/SampleCodeTests/FooTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SampleCodeLibrary;
+
+namespace SampleCodeTests
+{
+    [TestFixture]
+    public class FooTests
+    {
+        [Test]
+        public void FooTest1()
+        {
+            var sut = new Foo();
+            Assert.That(sut.GetTestedResult(false), Is.True);
+        }
+    }
+}
