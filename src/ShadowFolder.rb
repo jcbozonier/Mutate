30d2b85686a136a104c13a44ff5de3d4a4b69323
jcbozonier <darkxanthos@gmail.com>
2010-09-01 07:04:06 -0700

    indentation off

--- src/ShadowFolder.rb
+++ src/ShadowFolder.rb
@@ -7,9 +7,9 @@ class ShadowFolder
   def shadow
     cp_r @path_to_shadow + '\\.', @temp_root
 	
-	yield @temp_root
-	
-	rm_r @temp_root
+    yield @temp_root
+    
+    rm_r @temp_root
     raise "Folder not destroyed!!" if File.directory? @temp_root
   end
   
